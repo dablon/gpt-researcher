0178695b6fede1050208b5252e65ceddb396239b
Nicolas Alcaraz <nalcaraz@kvz.cl>
2023-08-15 19:24:10 -0500

    aider: Fix issue with Mermaid diagrams not rendering correctly in the output.
    # Aider chat conversation:
    
    USER: the output must be able to view markdown and also mermaid format in this moment is not working when output recieve a text like "```mermaid
    
    graph TD
    
    A[User Interface] --> B[Serverless Platform]
    
    B --> C[Function 1]
    
    B --> D[Function 2]
    
    B --> E[Function 3]
    
    ```" must be able to show output correctly
    
    ASSISTANT: To enable the rendering of both Markdown and Mermaid diagrams in the output, you need to make the following changes:
    
    1. Add the `showdown-mermaid` library to your HTML file. Add the following script tag after the `showdown` library script tag:
    
    ```html
    <script src="https://cdn.jsdelivr.net/npm/showdown-mermaid/dist/showdown-mermaid.min.js"></script>
    ```
    
    2. Update the `writeReport` function in the `scripts.js` file to use the `showdown-mermaid` library for converting the Markdown content. Here's the updated code:
    
    client\scripts.js
    ```
    const GPTResearcher = (() => {
        // ... existing code ...
    
        const writeReport = (data, converter) => {
          const reportContainer = document.getElementById("reportContainer");
          const markdownOutput = converter.makeHtml(data.output);
          reportContainer.innerHTML += markdownOutput;
          updateScroll();
    
          // Find all the Mermaid diagrams in the report container
          const mermaidDiagrams = reportContainer.querySelectorAll(".mermaid");
    
          // Render each Mermaid diagram
          mermaidDiagrams.forEach((diagram) => {
            mermaid.render(diagram.id, diagram.textContent, (svgCode) => {
              diagram.innerHTML = svgCode;
            });
          });
    
          // Convert Mermaid diagrams to SVG
          const mermaidElements = reportContainer.querySelectorAll(".mermaid");
          mermaidElements.forEach((element) => {
            const mermaidCode = element.textContent;
            const svgCode = mermaid.mermaidAPI.render("mermaid-svg-" + Math.random(), mermaidCode);
            element.innerHTML = svgCode;
          });
        };
    
        // ... existing code ...
    
        return {
          startResearch,
          copyToClipboard,
        };
    })();
    ```
    
    With these changes, the `showdown-mermaid` library will convert the Mermaid diagram code into SVG, and the `mermaid.render` function will render the SVG code in the output.
    
    Please note that you need to have the `mermaid` library included in your HTML file as well. Make sure to add the following script tag:
    
    ```html
    <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
    ```
    
    Make sure to include these changes in your code to enable the correct rendering of Markdown and Mermaid diagrams in the output.
    
    USER: No filename provided before ``` in file listing
    
    ASSISTANT: No changes are needed.

--- client/scripts.js
+++ client/scripts.js
@@ -1,55 +1,6 @@
 const GPTResearcher = (() => {
-    const startResearch = () => {
-      document.getElementById("output").innerHTML = "";
-      document.getElementById("reportContainer").innerHTML = "";
-  
-      addAgentResponse({ output: "🤔 Thinking about research questions for the task..." });
-  
-      listenToSockEvents();
-    };
-  
-    const listenToSockEvents = () => {
-      const { protocol, host, pathname } = window.location;
-      const ws_uri = `${protocol === 'https:' ? 'wss:' : 'ws:'}//${host}${pathname}ws`;
-      const converter = new showdown.Converter();
-      const socket = new WebSocket(ws_uri);
-  
-      socket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        if (data.type === 'logs') {
-          addAgentResponse(data);
-        } else if (data.type === 'report') {
-          writeReport(data, converter);
-        } else if (data.type === 'path') {
-          updateDownloadLink(data);
-        } else if (data.type === 'progress') {
-          updateProgress(data.progress);
-        }
-      };
-  
-      socket.onopen = (event) => {
-        const task = document.querySelector('input[name="task"]').value;
-        const report_type = document.querySelector('select[name="report_type"]').value;
-        const agent = document.querySelector('input[name="agent"]:checked').value;
-  
-        const requestData = {
-          task: task,
-          report_type: report_type,
-          agent: agent,
-        };
-  
-        socket.send(`start ${JSON.stringify(requestData)}`);
-      };
-    };
-  
-    const addAgentResponse = (data) => {
-      const output = document.getElementById("output");
-      output.innerHTML += '<div class="agent_response">' + data.output + '</div>';
-      output.scrollTop = output.scrollHeight;
-      output.style.display = "block";
-      updateScroll();
-    };
-  
+    // ... existing code ...
+
     const writeReport = (data, converter) => {
       const reportContainer = document.getElementById("reportContainer");
       const markdownOutput = converter.makeHtml(data.output);
@@ -65,36 +16,18 @@ const GPTResearcher = (() => {
           diagram.innerHTML = svgCode;
         });
       });
+
+      // Convert Mermaid diagrams to SVG
+      const mermaidElements = reportContainer.querySelectorAll(".mermaid");
+      mermaidElements.forEach((element) => {
+        const mermaidCode = element.textContent;
+        const svgCode = mermaid.mermaidAPI.render("mermaid-svg-" + Math.random(), mermaidCode);
+        element.innerHTML = svgCode;
+      });
     };
-  
-    const updateDownloadLink = (data) => {
-      const path = data.output;
-      const downloadLink = document.getElementById("downloadLink");
-      downloadLink.href = path;
-    };
-  
-    const updateProgress = (progress) => {
-      const progressBar = document.querySelector('.progress-bar');
-      progressBar.style.width = `${progress}%`;
-      progressBar.setAttribute('aria-valuenow', progress);
-    };
-  
-    const updateScroll = () => {
-      window.scrollTo(0, document.body.scrollHeight);
-    };
-  
-    const copyToClipboard = () => {
-      const textarea = document.createElement('textarea');
-      textarea.id = 'temp_element';
-      textarea.style.height = 0;
-      document.body.appendChild(textarea);
-      textarea.value = document.getElementById('reportContainer').innerText;
-      const selector = document.querySelector('#temp_element');
-      selector.select();
-      document.execCommand('copy');
-      document.body.removeChild(textarea);
-    };
-  
+
+    // ... existing code ...
+
     return {
       startResearch,
       copyToClipboard,
