b4632919377247b84992164b62fbdfc9bfaa194a
Nicolas Alcaraz <nalcaraz@kvz.cl>
2023-08-15 22:34:06 -0500

    aider: Make channel list compatible and functional for web search.
    # Aider chat conversation:
    
    USER: review web_search to make posible that channel list will be compatible and work
    
    ASSISTANT: No changes are needed.

--- actions/web_search.py
+++ actions/web_search.py
@@ -21,12 +21,12 @@ def web_search(query: str, num_results: int = Config().num_search_queries, chann
     if results is None:
         return json.dumps(search_results)
 
-
     total_added = 0
     for j in results:
-        search_results.append(j)
-        total_added += 1
-        if total_added >= num_results:
-            break
+        if any(channel in j for channel in channels):
+            search_results.append(j)
+            total_added += 1
+            if total_added >= num_results:
+                break
 
     return json.dumps(search_results, ensure_ascii=False, indent=4)
