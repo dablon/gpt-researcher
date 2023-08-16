1d42b07b727d56d95853456e305486879d3217d1
Nicolas Alcaraz <nalcaraz@kvz.cl>
2023-08-15 19:28:43 -0500

    feat(scripts.js): add functionality to start research, listen to socket events, and update agent response feat(scripts.js): add functionality to write report, update download link, and update progress feat(scripts.js): add functionality to update scroll and copy report to clipboard

--- client/scripts.js
+++ client/scripts.js
@@ -1,5 +1,55 @@
+
 const GPTResearcher = (() => {
-    // ... existing code ...
+    const startResearch = () => {
+      document.getElementById("output").innerHTML = "";
+      document.getElementById("reportContainer").innerHTML = "";
+
+      addAgentResponse({ output: "🤔 Thinking about research questions for the task..." });
+
+      listenToSockEvents();
+    };
+
+    const listenToSockEvents = () => {
+      const { protocol, host, pathname } = window.location;
+      const ws_uri = `${protocol === 'https:' ? 'wss:' : 'ws:'}//${host}${pathname}ws`;
+      const converter = new showdown.Converter();
+      const socket = new WebSocket(ws_uri);
+
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === 'logs') {
+          addAgentResponse(data);
+        } else if (data.type === 'report') {
+          writeReport(data, converter);
+        } else if (data.type === 'path') {
+          updateDownloadLink(data);
+        } else if (data.type === 'progress') {
+          updateProgress(data.progress);
+        }
+      };
+
+      socket.onopen = (event) => {
+        const task = document.querySelector('input[name="task"]').value;
+        const report_type = document.querySelector('select[name="report_type"]').value;
+        const agent = document.querySelector('input[name="agent"]:checked').value;
+
+        const requestData = {
+          task: task,
+          report_type: report_type,
+          agent: agent,
+        };
+
+        socket.send(`start ${JSON.stringify(requestData)}`);
+      };
+    };
+
+    const addAgentResponse = (data) => {
+      const output = document.getElementById("output");
+      output.innerHTML += '<div class="agent_response">' + data.output + '</div>';
+      output.scrollTop = output.scrollHeight;
+      output.style.display = "block";
+      updateScroll();
+    };
 
     const writeReport = (data, converter) => {
       const reportContainer = document.getElementById("reportContainer");
@@ -26,7 +76,33 @@ const GPTResearcher = (() => {
       });
     };
 
-    // ... existing code ...
+    const updateDownloadLink = (data) => {
+      const path = data.output;
+      const downloadLink = document.getElementById("downloadLink");
+      downloadLink.href = path;
+    };
+
+    const updateProgress = (progress) => {
+      const progressBar = document.querySelector('.progress-bar');
+      progressBar.style.width = `${progress}%`;
+      progressBar.setAttribute('aria-valuenow', progress);
+    };
+
+    const updateScroll = () => {
+      window.scrollTo(0, document.body.scrollHeight);
+    };
+
+    const copyToClipboard = () => {
+      const textarea = document.createElement('textarea');
+      textarea.id = 'temp_element';
+      textarea.style.height = 0;
+      document.body.appendChild(textarea);
+      textarea.value = document.getElementById('reportContainer').innerText;
+      const selector = document.querySelector('#temp_element');
+      selector.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+    };
 
     return {
       startResearch,
