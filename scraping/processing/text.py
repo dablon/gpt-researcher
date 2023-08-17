648479a511509cfd89cd22c751fcd0f6b800a251
Nicolas Alcaraz <nalcaraz@kvz.cl>
2023-08-16 23:40:29 -0500

    refactor(text.py): replace markdown library with md2pdf for converting markdown to pdf

--- scraping/processing/text.py
+++ scraping/processing/text.py
@@ -7,7 +7,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from config import Config
 from gpt_researcher_old.retriever.llm_utils import create_chat_completion
 import os
-import markdown
+from md2pdf.core import md2pdf
 
 
 def split_text(text: str, max_length: int = 8192) -> Generator[str, None, None]:
@@ -166,9 +166,8 @@ def read_txt_files(directory):
 
 
 def md_to_pdf(input_file, output_file):
-    with open(input_file, 'r') as md_file:
-        md_content = md_file.read()
-
-    html_content = markdown.markdown(md_content)
-
-    pdfkit.from_string(html_content, output_file, css='./processing/styles.css')
+    md2pdf(output_file,
+           md_content=None,
+           md_file_path=input_file,
+           css_file_path=None,
+           base_url=None)
